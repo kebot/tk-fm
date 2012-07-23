d4a443b69ce702873a4c8fa7939f18fd643bd87e
Keith Yao <i@yaofur.com>
2012-07-24 00:08:42 +0800

    add a redis powered session

--- turkeyfm/__init__.py
+++ turkeyfm/__init__.py
@@ -3,8 +3,15 @@
 
 from flask import Flask
 from flask.ext.mako import MakoTemplates
+from utils.rsession import RedisSessionInterface
+import redis
+
+# will be configed
+redis_client = redis.Redis('localhost')
 
 app = Flask(__name__, static_folder='public')
+
+app.session_interface = RedisSessionInterface(redis=redis_client, prefix='turfm-session:')
 
 app.config.update({
     'MAKO_INPUT_ENCODING': 'utf-8',
