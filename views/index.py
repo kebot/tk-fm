ffbbdb5ca5584f2d2e01937356de838ae78e3db3
Keith Yao <i@yaofur.com>
2013-05-03 21:34:21 +0800

    restful mixin(unused)

--- views/index.py
+++ views/index.py
@@ -3,12 +3,33 @@
 
 from flask import render_template, session, redirect, url_for, request
 from turkeyfm import app
+from turkeyfm.models import Room as RoomModel
 
 @app.route('/', methods=['GET'])
 def index():
     """docstring for index"""
     return render_template('index.html')
 
+# url-pattern '/room/:room.uuid'
+# ------------------------ room crud ------------------------------
+@app.route('/j/room/<path:rid>', methods=[
+    'GET', 'POST', 'UPDATE', 'DELETE', 'PATCH'])
+def restful_room(rid):
+    response = None
+    model = RoomModel(id=rid)
+    model.fetch()
+
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
+    elif request.method == 'UPDATE':
+        pass
+    elif request.method == 'DELETE':
+        pass
+
+
+# ------------------------ misc -----------------------------------
 @app.route('/crossdomain.xml')
 def crossdomain():
     return redirect(url_for('static', filename="crossdomain.xml"))
