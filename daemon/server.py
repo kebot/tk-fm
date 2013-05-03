2c43c2be64ba944498edcb8abe7332b48e7dc8b5
Keith Yao <i@yaofur.com>
2013-05-03 21:30:19 +0800

    gevent based worker with daemon

--- daemon/server.py
+++ daemon/server.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from turkeyfm.daemon.manager import Manager
 from turkeyfm.daemon.worker import FavSongWorker
 
 def run_server():
-    import threading
-    make = Manager()
-    make.add_worker(FavSongWorker)
-    threading.Thread(target=make.love).start()
+    import gevent
+    worker = gevent.spawn(FavSongWorker)
+    gevent.joinall([worker,])
+
+    #import threading
+    #make = Manager()
+    #make.add_worker(FavSongWorker)
+    #threading.Thread(target=make.love).start()
 
 if __name__ == '__main__':
     run_server()
